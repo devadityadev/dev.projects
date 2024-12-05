4b06f9f5ab38641ad81b312164ad3ba0eca807ca
devadityadev <raghavendra91440@gmail.com>
2024-12-04 21:06:36 -0800

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function cancel(){
 const texts = [
     "DEVELOPER",
     "security",
-    "akshay"
+    "sheild"
 ]
 let speed  =100;
 const textElements = document.querySelector(".typewriter-text");
