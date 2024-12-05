648b2b4922b2fc9960970a2203bf43fc7c201bf0
devadityadev <raghavendra91440@gmail.com>
2024-12-04 21:03:33 -0800

    Update script.js

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ function cancel(){
 // Typewriter Effect
 const texts = [
     "DEVELOPER",
-    "security",
-    "sheild"
+    "DESIGNER",
+    "YOUTUBER"
 ]
 let speed  =100;
 const textElements = document.querySelector(".typewriter-text");
@@ -36,21 +36,5 @@ function eraseText(){
         charcterIndex = 0;
         setTimeout(typeWriter, 500)
     }
-    function showBlackScreen() {
-    const overlay = document.createElement("div");
-    overlay.id = "neon-overlay";
-    overlay.innerHTML = `
-        <div class="neon-text">Welcome to Neon Mode</div>
-    `;
-    document.body.appendChild(overlay);
-    overlay.style.display = "flex";
-
-    // Close the overlay when clicked
-    overlay.addEventListener("click", () => {
-        overlay.style.display = "none";
-        document.body.removeChild(overlay);
-    });
-}
-
 }
 window.onload = typeWriter
